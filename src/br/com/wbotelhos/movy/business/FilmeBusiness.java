e57c49d1bb20dcb4311664b4447ae8cb839de8a3
Washington Botelho <wbotelhos@gmail.com>
2011-07-07 21:40:16 -0300

    Adicionado classe genérica para o CRUD. Removido .class sucks.

--- src/br/com/wbotelhos/movy/business/FilmeBusiness.java
+++ src/br/com/wbotelhos/movy/business/FilmeBusiness.java
@@ -1,41 +1,16 @@
 package br.com.wbotelhos.movy.business;
 
-import java.util.Collection;
-
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 
 import br.com.caelum.vraptor.ioc.Component;
+import br.com.wbotelhos.movy.business.common.GenericBusiness;
 import br.com.wbotelhos.movy.model.Filme;
 
 @Component
-public class FilmeBusiness {
-
-	private final EntityManager manager;
+public class FilmeBusiness extends GenericBusiness<Filme> {
 
 	public FilmeBusiness(EntityManager manager) {
-	  this.manager = manager;
-	}
-
-	public Collection<Filme> loadAll() {
-		Query query = manager.createQuery("from Filme");
-
-		@SuppressWarnings("unchecked")
-		Collection<Filme> resultList = query.getResultList();
-
-		return resultList;
-	}
-
-	public Filme loadById(Long id) {
-		return manager.find(Filme.class, id);
-	}
-
-	public void remove(Filme filme) {
-	    manager.remove(manager.getReference(Filme.class, filme.getId()));
-	}
-
-	public Filme save(Filme filme) {
-		return manager.merge(filme);
+	  super(manager);
 	}
 
 }
---
+++ src/br/com/wbotelhos/movy/business/common/GenericBusiness.java
@@ -0,0 +1,46 @@
+package br.com.wbotelhos.movy.business.common;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.Collection;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.wbotelhos.movy.model.common.AbstractEntity;
+
+public abstract class GenericBusiness<T extends AbstractEntity> {
+
+	protected final EntityManager manager;
+	protected final Class<T> clazz;
+
+	protected GenericBusiness(EntityManager manager) {
+		this.manager = manager;
+
+		@SuppressWarnings("unchecked")
+		Class<T> clazz = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+
+		this.clazz = clazz;
+	}
+
+	public Collection<T> loadAll() {
+		Query query = manager.createQuery("from " + clazz.getName());
+
+		@SuppressWarnings("unchecked")
+		Collection<T> resultList = query.getResultList();
+
+		return resultList;
+	}
+
+	public T loadById(Long id) {
+		return manager.find(clazz, id);
+	}
+
+	public void remove(T entity) {
+		manager.remove(manager.getReference(clazz, entity.getId()));
+	}
+
+	public T save(T entity) {
+		return manager.merge(entity);
+	}
+
+}
